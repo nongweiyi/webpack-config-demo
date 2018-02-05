9ae9fd32e8e3e413c34318ceb3196bb59694d4dc
unknown <nwy@xinhe.com>
2018-02-05 10:12:24 +0800

    添加压缩js、css配置

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     // 配置入口
@@ -16,32 +18,32 @@ module.exports = {
         filename: 'js/[name]-[hash:5].js',
         publicPath: '/',
     },
-   
+
     module: {
         loaders: [
             //解析.js
-            {   
-                test: '/\.js$/',  
-                loader: 'babel',  
-                exclude: path.resolve(__dirname, 'node_modules'), 
+            {
+                test: '/\.js$/',
+                loader: 'babel',
+                exclude: path.resolve(__dirname, 'node_modules'),
                 include: path.resolve(__dirname, 'src'),
                 query: {
-                    presets: ['env'] 
+                    presets: ['env']
                 }
             },
             // css处理
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader' 
-               
+                loader: 'style-loader!css-loader'
+
             },
             // less处理
-            { 
+            {
                 test: /\.less$/,
                 loader: 'style-loader!css-loader!less-loader'
             },
             // 图片处理
-            { 
+            {
                 test: /\.(png|jpg|gif|svg)$/,
                 loader: 'file-loader',
 
@@ -50,29 +52,42 @@ module.exports = {
                 },
             },{
                 test: /\.(htm|html)$/i,
-                use:[ 'html-withimg-loader'] 
+                use:[ 'html-withimg-loader']
             }
         ]
     },
     plugins: [
         new ExtractTextPlugin(__dirname + '/assert/css/common.less'),
-        //  minify:{
-        //     removeComments: true,//删除注释
-        //     collapseWhitespace:true//删除空格
-        // }
+
+        new UglifyJsPlugin(),//压缩js
+       //压缩css
+       new OptimizeCssAssetsPlugin({
+           assetNameRegExp: /\.optimize\.css$/g,
+           cssProcessor: require('cssnano'),
+           cssProcessorOptions: { discardComments: {removeAll: true } },
+           canPrint: true
+         }),
         new HtmlWebpackPlugin({
             filename: __dirname + '/dist/about.html',
             inject: 'head',
             template: 'html-withimg-loader!'+__dirname + "/src/pages/about/about.html",
             chunks: ['about'],
-            inlineSource: '.(js|css)$'
+            inlineSource: '.(js|css)$',
+             minify:{
+                removeComments: true,//删除注释
+                collapseWhitespace:true//删除空格
+            }
         }),
         new HtmlWebpackPlugin({
             inject: 'head',
             filename: __dirname + '/dist/contact.html',
             template: __dirname + "/src/pages/contact/contact.html",
             chunks: ['contact'],
-            inlineSource: '.(js|css)$'
+            inlineSource: '.(js|css)$',
+            minify:{
+               removeComments: true,//删除注释
+               collapseWhitespace:true//删除空格
+           }
         }),
         //设置每一次build之前先删除dist
         new CleanWebpackPlugin(
